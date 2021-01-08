057bf9292ae65c2f072f3eef62ce9860d1d7f58d
JyotsnaKumari <jyotsnakumari108@gmail.com>
2021-01-08 17:26:37 +0530

    Add files via upload
    added classes

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JewelleryStore.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public UserType userType { get; set; }
+        public double GoldWeight { get; set; }
+        public double GoldRate { get; set; }
+        public double DiscountInPercentage { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
